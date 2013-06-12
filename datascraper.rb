b5858eae053ebd5b5ddb34250e595966284fd3c3
ZeroFries <sgarvagh@uoguelph.ca>
2013-06-12 16:00:49 -0400

    fixed data

--- datascraper.rb
+++ datascraper.rb
@@ -5,12 +5,12 @@ require "open-uri"
 file = File.open("./data.txt", 'w')
 temp = ""
  
-2013.downto(1995) do |year|
+2013.downto(1985) do |year|
+	file.write("#{year} ")
 	url = "http://www.wunderground.com/history/airport/CYTZ/#{year}/6/12/WeeklyHistory.html"
 	page = Nokogiri::HTML(open(url))
 	(0..2).each do |i|
 		temp = page.css("#historyTable").css(".b")[i*3 + 1].text
-		file.write("#{year} ")
 	  file.write(temp + " ") if !temp.nil? && !temp.empty?
 	end
   file.write("\n")
